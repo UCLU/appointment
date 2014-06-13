cca52177fc0c05fed22885e584df94b2c5767d25
Patrick Dawkins <p.dawkins@ucl.ac.uk>
2014-06-13 11:58:17 +0100

    Use fields instead of custom properties.

---
+++ modules/slot/includes/appointment-slot.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display an appointment slot.
+ */
+?>
+<div id="appointment-slot-<?php print $slot->slot_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php if (!$page && $content['field_appointment_time']): ?>
+    <h2><?php print render($content['field_appointment_time']); ?></h2>
+  <?php elseif (!$page && $label): ?>
+    <h2><?php print $label; ?></h2>
+  <?php endif; ?>
+
+  <div class="content">
+    <?php print render($content); ?>
+  </div>
+
+</div>
