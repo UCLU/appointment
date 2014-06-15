8e018bba40d668273ada6d97299c9e2f0563f658
Patrick Dawkins <p.dawkins@ucl.ac.uk>
2014-06-15 16:12:49 +0100

    booking: use form instead of link; allow cancelling; add unique key on slot_id/uid.

--- modules/slot/includes/appointment-slot.tpl.php
+++ modules/slot/includes/appointment-slot.tpl.php
@@ -6,9 +6,9 @@
 ?>
 <div id="appointment-slot-<?php print $slot->slot_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php if (!$page && !empty($content['field_appointment_time'])): ?>
+  <?php if ($teaser && !empty($content['field_appointment_time'])): ?>
     <h2><?php print render($content['field_appointment_time']); ?></h2>
-  <?php elseif (!$page && $label): ?>
+  <?php elseif ($teaser && $label): ?>
     <h2><?php print $label; ?></h2>
   <?php endif; ?>
 
