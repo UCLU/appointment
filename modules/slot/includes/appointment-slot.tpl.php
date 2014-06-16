fc8ea7bcce52e5079cc537893941fb16fc399033
Patrick Dawkins <p.dawkins@ucl.ac.uk>
2014-06-16 15:52:50 +0100

    Allow booking for anonymous users.

--- modules/slot/includes/appointment-slot.tpl.php
+++ modules/slot/includes/appointment-slot.tpl.php
@@ -7,9 +7,9 @@
 <div id="appointment-slot-<?php print $slot->slot_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if ($teaser && !empty($content['field_appointment_time'])): ?>
-    <h2><?php print render($content['field_appointment_time']); ?></h2>
+    <h2><a href="<?php print $url; ?>"><?php print render($content['field_appointment_time']); ?></a></h2>
   <?php elseif ($teaser && $label): ?>
-    <h2><?php print $label; ?></h2>
+    <h2><a href="<?php print $url; ?>"><?php print $label; ?></a></h2>
   <?php endif; ?>
 
   <div class="content">
